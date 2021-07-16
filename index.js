96977099a9f1b3a29eefbd31d81679120c5eaa15
jassi1110 <jasnoorsingh11@gmail.com>
2021-07-17 01:04:19 +0530

    bacspace

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 buttons = document.querySelectorAll('button');
-let division = document.getElementById('divison')
 let screenValue = '';
 let historyValue = '';
+let back = document.getElementById('back').className
+console.log(back)
 let screen = document.getElementById('output-value');
 let history = document.getElementById('history-value');
 for (item of buttons) {
     item.addEventListener('click', (e) => {
         buttonText = e.target.innerText;
+        backSpace = e.target.className
         // console.log('Button text is ', buttonText);
+        // console.log('Button text is ', backSpace);
         if (buttonText == 'x') {
             buttonText = '*';
             screenValue += buttonText;
@@ -24,12 +27,21 @@ for (item of buttons) {
             screen.innerText = eval(screenValue);
             screenValue = screen.innerText
         }
+
+        else if(backSpace==back || backSpace=='bi bi-backspace-fill'){
+            let n = screenValue.length
+            console.log(n)
+            screenValue = screenValue.slice(0,n-1)
+            screen.innerText = screenValue
+            // console.log(str)
+        }
         
         else {
             history.innerText += buttonText
             screenValue += buttonText;
             screen.innerText = screenValue;
         }
+    // console.log(back)
     })
 }
 
