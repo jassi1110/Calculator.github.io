dedb05d5c8faf55e4dcf62c25e7eaec1bab5f3d5
jassi1110 <jasnoorsingh11@gmail.com>
2021-07-16 14:37:21 +0530

    my calculator App

---
+++ js/index.js
@@ -0,0 +1,34 @@
+buttons = document.querySelectorAll('button');
+let division = document.getElementById('divison')
+let screenValue = '';
+let historyValue = '';
+let screen = document.getElementById('output-value');
+let history = document.getElementById('history-value');
+for (item of buttons) {
+    item.addEventListener('click', (e) => {
+        buttonText = e.target.innerText;
+        // console.log('Button text is ', buttonText);
+        if (buttonText == 'x') {
+            buttonText = '*';
+            screenValue += buttonText;
+            history.innerText += buttonText
+            screen.innerText = screenValue;
+        }
+        else if (buttonText == 'C') {
+            screenValue = "";
+            history.innerText = ''
+            screen.innerText = screenValue;
+        }
+        else if (buttonText == '=') {
+            history.innerText = ''
+            screen.innerText = eval(screenValue);
+        }
+        
+        else {
+            history.innerText += buttonText
+            screenValue += buttonText;
+            screen.innerText = screenValue;
+        }
+    })
+}
+
